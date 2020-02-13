39aabb12330945a774e76c64adff420899c200ac
CISCO LV1013 <lv1013@itson.edu.mx>
2020-02-13 11:12:31 -0700

    Avance 1

---
+++ src/DAO/SocioDAOImpl.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import capas.ConnectionFactory;
+import capas.Socio;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.List;
+
+/**
+ *
+ * @author lv1013
+ */
+public class SocioDAOImpl implements SocioDAO {
+    
+    private final ConnectionFactory CONNECTION_FACTORY;
+    
+    public SocioDAOImpl(ConnectionFactory CONNECTION_FACTORY) {
+        this.CONNECTION_FACTORY = CONNECTION_FACTORY;
+    }
+    
+    @Override
+    public Socio find(int id) throws Exception {
+        final String SQL = "SELECT id, nombre, direccion "
+                + "FROM socio WHERE id = ?";
+        try (Connection connection = this.CONNECTION_FACTORY.getConnection();
+            PreparedStatement statement = connection.prepareStatement(SQL);){
+            
+        }
+        
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Socio> getAll() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void add(Socio socio) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(Socio socio) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(int id) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
